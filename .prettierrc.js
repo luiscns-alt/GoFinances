9219504c3e26e4017bce335aec784febdae40e93
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-06 11:05:09 -0300

    Feat: Config Typescript.

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    jsxBracketSameLine: true,
+}
