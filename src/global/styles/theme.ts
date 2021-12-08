34fea91869acf443646c91cc33b443cd79d3d05a
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-07 23:27:13 -0300

    Feat: Utilizando Fontes Personalizadas.

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -10,6 +10,11 @@ export default {
         shape: '#FFFFFF',
         title: '#363F5F',
         text: '#969CB2',
-        background: '#F0F2F5'
-    }
-}
+        background: '#F0F2F5',
+    },
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
+    },
+};
