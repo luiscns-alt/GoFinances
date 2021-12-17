4f1192c3cdb9c00b07fc6ccf9fa646c7d5f230b4
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-17 00:25:14 -0300

    Feature: Tipando e acessando propriedades no Styled Components

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -10,16 +10,29 @@ import {
     LastTransition,
 } from './styles';
 
-export function HighlightCard() {
+interface Props {
+    title: string;
+    amount: string;
+    lastTransition: string;
+    type: 'up' | 'down' | 'total';
+}
+
+const icon = {
+    up: 'arrow-up-cicle',
+    down: 'arrow-down-cicle',
+    total: 'doll-sing',
+};
+
+export function HighlightCard({ type, title, amount, lastTransition }: Props) {
     return (
-        <Container>
+        <Container type={type}>
             <Header>
-                <Title>Entrada</Title>
-                <Icon name="arrow-up-circle" />
+                <Title type={type}>{title}</Title>
+                <Icon name={icon[type]} type={type} />
             </Header>
             <Footer>
-                <Amount>R$ 17.000,00</Amount>
-                <LastTransition>Ùltima entrada dia 13 de abril</LastTransition>
+                <Amount type={type}>{amount}</Amount>
+                <LastTransition type={type}> {lastTransition}</LastTransition>
             </Footer>
         </Container>
     );
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -35,9 +35,24 @@ export function Dashboard() {
             </Header>
 
             <HighlightCards>
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransition="Última entrada dia 1 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransition="Última saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransition="01 à 16 de abril"
+                />
             </HighlightCards>
         </Container>
     );
