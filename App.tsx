9496806fb725c065a8dba9600a463bd58b6fe9f4
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-06 14:20:57 -0300

    feat: interface de autenticação

--- App.tsx
+++ App.tsx
@@ -22,6 +22,8 @@ import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 
+import { SignIn } from './src/screens/SignIn';
+
 export default function App() {
     const [fontsLoaded] = useFonts({
         Poppins_400Regular,
@@ -35,7 +37,7 @@ export default function App() {
         <ThemeProvider theme={theme}>
             <NavigationContainer>
                 <StatusBar barStyle="light-content" />
-                <AppRoutes />
+                <SignIn />
             </NavigationContainer>
         </ThemeProvider>
     );
