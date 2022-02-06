ec04b3a9dcc7e6f925fc271894d9107176e04369
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-05 21:00:19 -0300

    Feat: Estilização do filtro por mês.

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -3,11 +3,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { VictoryPie } from 'victory-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useTheme } from 'styled-components';
 
 import { HistoryCard } from '../../components/HistoryCard';
 
-import { Container, Header, Title, Content, ChartContainer } from './styles';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    ChartContainer,
+    MonthSelect,
+    MonthSelectButton,
+    MonthSelectIcon,
+    Month,
+} from './styles';
 
 import { categories } from '../../utils/categories';
 
@@ -97,7 +108,23 @@ export function Resume() {
                 <Title>Resumo por categoria</Title>
             </Header>
 
-            <Content>
+            <Content
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingHorizontal: 24,
+                    paddingBottom: useBottomTabBarHeight(),
+                }}>
+                <MonthSelect>
+                    <MonthSelectButton>
+                        <MonthSelectIcon name="chevron-left" />
+                    </MonthSelectButton>
+
+                    <Month>Fevereiro</Month>
+
+                    <MonthSelectButton>
+                        <MonthSelectIcon name="chevron-right" />
+                    </MonthSelectButton>
+                </MonthSelect>
                 <ChartContainer>
                     <VictoryPie
                         data={totalByCategories}
