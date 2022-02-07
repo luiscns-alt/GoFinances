9b30af7ff21eac5cbad75020b6ec2fe82e1e38c4
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-07 00:11:21 -0300

    feat: opções de autenticação na plataforma

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ActivityIndicator, Alert } from 'react-native';
+import { ActivityIndicator, Alert, Platform } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useTheme } from 'styled-components';
 
@@ -72,11 +72,13 @@ export function SignIn() {
                         svg={GoogleSvg}
                         onPress={handlerSignInWithGoogle}
                     />
-                    <SingInSocialButton
-                        title="Entrar com Apple"
-                        svg={AppleSvg}
-                        onPress={handleSignInWithApple}
-                    />
+                    {Platform.OS === 'ios' && (
+                        <SingInSocialButton
+                            title="Entrar com Apple"
+                            svg={AppleSvg}
+                            onPress={handleSignInWithApple}
+                        />
+                    )}
                 </FooterWrapper>
 
                 {isLoading && (
